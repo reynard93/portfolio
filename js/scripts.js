5c08b0d6e8818ad0dab498cb8c6cc1f7233c96a6
reyanrd93 <yylee.rey@gmail.com>
2019-07-31 21:22:13 +0800

    myfinalpiece

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,58 @@
-$(document).ready(function () {
-  $('#portfolio-button').click(function () {
-    $('html, body').animate({
-      scrollTop: $('#portfolio-section').offset().top
-    }, 1500)
+$(document).ready(function() {
+  $("#portfolio-button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#portfolio-section").offset().top
+      },
+      1500
+    );
   });
-})
+  const portImages = document.querySelectorAll(".loader img");
+
+  const portContainer = document.querySelector("#portfolio-section");
+
+  var is_mobile = false;
+
+  if ($("#some-element").css("display") == "none") {
+    is_mobile = true;
+  }
+
+  function debounce(func, wait = 20, immeditate = true) {
+    var timeout;
+    return function() {
+      var context = this,
+        args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immeditate) func.apply(context, args);
+      };
+      var callNow = immeditate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function checkSlide(e) {
+    portImages.forEach(portImage => {
+      const slideInAt = window.scrollY + window.innerHeight;
+      var isShown;
+      if (is_mobile == true) {
+        isShown = slideInAt > portContainer.offsetTop - portImage.height * 8;
+        console.log(portContainer.offsetTop - portImage.height * 8);
+      } else {
+        isShown = slideInAt > portContainer.offsetTop + portImage.height ;
+      }
+      if (isShown) {
+        $(".loader").addClass("hidden");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", debounce(checkSlide));
+
+  $(".portfolio-card").on("click", function() {
+    const url = $(this).data("url");
+    window.open(url, "portfolioPage");
+  });
+});
